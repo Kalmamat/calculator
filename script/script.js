9a0e1ebe5891aa0c5901c92eab6fcc4a87f805ac
Kalmamat <mazhievk@bk.ru>
2021-03-18 09:51:18 +0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,65 @@
+// let viewer = document.querySelector('.viewer')
+// let button = document.querySelectorAll('button')
+//
+//
+// button.forEach(function (btn){
+//     btn.addEventListener('click', view)
+// })
+//
+// let num = ''
+// let equation = ''
+// let oneChk = false
+//
+//
+// function view(event) {
+//     const target = event.target.innerText
+//     if (target === 'AC'){
+//         num = ""
+//         equation = ""
+//         viewer.innerText = "0"
+//     }else if (['+', '-','*', '/'].includes(target)){
+//         if (oneChk === false) {
+//             equation = equation + target
+//             num = ""
+//             oneChk = true
+//         } else {
+//             viewer.innerText = eval(equation)
+//             equation = equation + target
+//             num = ""
+//
+//         }
+//     } else if (target === '=') {
+//         viewer.innerText = eval(equation)
+//         equation = eval(equation)
+//     } else {
+//        equation = equation + target
+//         num = num + target
+//         viewer.textContent = num
+//     }
+// }
+
+let out = document.querySelector('.viewer')
+let buttons = document.querySelectorAll('.btn')
+
+let num = ''
+let expression = ''
+let Check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
