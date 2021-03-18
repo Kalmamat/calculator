677723dc4c9fb844960bdd2b58ec99f1f8c31b7c
Kalmamat <mazhievk@bk.ru>
2021-03-18 09:58:27 +0400

    started

--- script/script.js
+++ script/script.js
@@ -1,49 +1,43 @@
-// let viewer = document.querySelector('.viewer')
-// let button = document.querySelectorAll('button')
-//
-//
-// button.forEach(function (btn){
-//     btn.addEventListener('click', view)
-// })
-//
-// let num = ''
-// let equation = ''
-// let oneChk = false
-//
-//
-// function view(event) {
-//     const target = event.target.innerText
-//     if (target === 'AC'){
-//         num = ""
-//         equation = ""
-//         viewer.innerText = "0"
-//     }else if (['+', '-','*', '/'].includes(target)){
-//         if (oneChk === false) {
-//             equation = equation + target
-//             num = ""
-//             oneChk = true
-//         } else {
-//             viewer.innerText = eval(equation)
-//             equation = equation + target
-//             num = ""
-//
-//         }
-//     } else if (target === '=') {
-//         viewer.innerText = eval(equation)
-//         equation = eval(equation)
-//     } else {
-//        equation = equation + target
-//         num = num + target
-//         viewer.textContent = num
-//     }
-// }
+let viewer = document.querySelector('.viewer')
+let button = document.querySelectorAll('button')
 
-let out = document.querySelector('.viewer')
-let buttons = document.querySelectorAll('.btn')
+
+button.forEach(function (btn){
+    btn.addEventListener('click', view)
+})
 
 let num = ''
-let expression = ''
-let Check
+let equation = ''
+let oneChk = false
+
+
+function view(event) {
+    const target = event.target.innerText
+    if (target === 'AC'){
+        num = ""
+        equation = ""
+        viewer.innerText = "0"
+    }else if (['+', '-','*', '/'].includes(target)){
+        if (oneChk === false) {
+            equation = equation + target
+            num = ""
+            oneChk = true
+        } else {
+            viewer.innerText = eval(equation)
+            equation = equation + target
+            num = ""
+
+        }
+    } else if (target === '=') {
+        viewer.innerText = eval(equation)
+        equation = eval(equation)
+    } else {
+       equation = equation + target
+        num = num + target
+        viewer.textContent = num
+    }
+}
+
 
 
 
